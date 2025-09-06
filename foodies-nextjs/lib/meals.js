13c6d7eb59cd06ea1e98877d4f2ff4bc2d23b462
Sergiu <info@webdesign7.co.uk>
2025-09-06 09:25:31 +0100

    new pages and new functions

--- foodies-nextjs/lib/meals.js
+++ foodies-nextjs/lib/meals.js
@@ -3,8 +3,12 @@ import xss from 'xss';
 import slugify from 'slugify';
 import fs from 'node:fs'
 import { Buffer } from 'node:buffer'; // Use Node's Buffer
-
+import { S3 } from '@aws-sdk/client-s3'
 const db = sql('meals.db');
+
+const s3 = new S3({
+    region: 'eu-west-2'
+});
 
 export async function getMeals() {
     return db.prepare('SELECT * FROM meals').all();
@@ -20,14 +24,22 @@ export default async function saveMeal(meal) {
     const filename = `${meal.slug}.${extension}`;
 
     // Ensure the images directory exists
-    await fs.promises.mkdir('public/images', { recursive: true });
+    //await fs.promises.mkdir('public/images', { recursive: true });
 
     // Convert the web ArrayBuffer to a Node Buffer and write the file
     const arrayBuffer = await meal.image.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
-    await fs.promises.writeFile(`public/images/${filename}`, buffer);
+    //const buffer = Buffer.from(arrayBuffer);
+    //await fs.promises.writeFile(`public/images/${filename}`, buffer);
 
-    meal.image = `/images/${filename}`;
+
+    await s3.putObject({
+        Bucket: 'react-meals-project',
+        Key: filename,
+        Body: Buffer.from(arrayBuffer),
+        ContentType: meal.image.type,
+    });
+
+    meal.image = filename;
 
     return db
         .prepare(
--- foodies-nextjs/next.config.js
+++ foodies-nextjs/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['react-meals-project.s3.eu-west-2.amazonaws.com'],
+    }
+}
 
 module.exports = nextConfig
